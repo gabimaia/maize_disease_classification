7274cf7136c63d68b5881ba2ff1f630dabb8a4fe
Gabriel Maia <gbrlmdm@gmail.com>
2022-07-28 01:28:19 -0300

    update main

--- source/api/main.py
+++ source/api/main.py
@@ -13,54 +13,18 @@ import joblib
 import os
 import wandb
 import sys
-from source.api.pipeline import FeatureSelector, CategoricalTransformer, NumericalTransformer
-
-# global variables
-setattr(sys.modules["__main__"], "FeatureSelector", FeatureSelector)
-setattr(sys.modules["__main__"], "CategoricalTransformer", CategoricalTransformer)
-setattr(sys.modules["__main__"], "NumericalTransformer", NumericalTransformer)
 
 # name of the model artifact
-artifact_model_name = "Decision_tree_heart_disease/model_export:latest"
+model = "jordaogabriel/model_export:latest"
 
 # initiate the wandb project
-run = wandb.init(project="Decision_tree_heart_disease",job_type="api")
+run = wandb.init(project="jordaogabriel",job_type="api")
 
 # create the api
 app = FastAPI()
 
 # declare request example data using pydantic
 # a person in our dataset has the following attributes
-class Person(BaseModel):
-    Age: int
-    Sex: str
-    ChestPainType: str
-    RestingBP: int
-    Cholesterol: int
-    FastingBS: int
-    RestingECG: str	
-    MaxHR: int
-    ExerciseAngina: str	
-    Oldpeak: float	
-    ST_Slope: str
-
-    class Config:
-        schema_extra = {
-            "example": {
-                "Age": 72,
-                "Sex": 'M',
-                "ChestPainType": 'NAP',
-                "RestingBP": 120,
-                "Cholesterol": 304,
-                "FastingBS": 1,
-                "RestingECG": 'Normal',
-                "MaxHR": 120,
-                "ExerciseAngina": 'Y',
-                "Oldpeak": 1.0,
-                "ST_Slope": 'Up'
-            }
-        }
-  
 
 def predict(image: Image.Image):
     image = np.asarray(image.resize((224, 224)))[..., :3]
